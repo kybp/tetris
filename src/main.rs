a71f051953d01c9aaed3fd462c8e17de74ec867f
Kyle Brown <picokyle@gmail.com>
2017-01-03 07:46:29 -0700

    Give 0 points for no lines cleared

--- src/main.rs
+++ src/main.rs
@@ -162,7 +162,8 @@ fn clear_filled_lines(
         1 =>  100,
         2 =>  200,
         3 =>  500,
-        _ => 1000,
+        4 => 1000,
+        _ =>    0,
     };
     score.lines += lines_cleared;
 }
